5f404e7f4f294983347a5e9c063b945037e928b3
zhanglongxia <zhanglongxia@google.com>
2021-09-23 16:09:45 +0800

    Add Android.bp support
    Bug: b/200911295
    Test: Run the command "make otbr-agent" to build otbr-agent.
    Change-Id: I98928ac1ae80e6cbbab635ccc85353efc0bb37f7

---
+++ src/android/config-android-version-gen.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+#  Copyright (c) 2021, The OpenThread Authors.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#  3. Neither the name of the copyright holder nor the
+#     names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+
+#  Description:
+#    This script generates the otbr-agent version header file needed by Android.bp.
+#
+
+set -euo pipefail
+
+main()
+{
+    # "Usage: config-android-version-gen.sh < src/android/otbr-config-android-version.h.in > otbr-config-android-version.h"
+
+    OTBR_VERSION_GEN_FILE="$0"
+    OTBR_VERSION_GEN_FILE_PATH=$(dirname "${OTBR_VERSION_GEN_FILE}")
+    OTBR_GIT_VERSION=$(cd ${OTBR_VERSION_GEN_FILE_PATH} && git describe --dirty --always)
+
+    sed -e s/@OTBR_SOURCE_VERSION@/"${OTBR_GIT_VERSION}"/
+}
+
+main "$@"
