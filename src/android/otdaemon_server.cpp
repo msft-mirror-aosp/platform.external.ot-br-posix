buddman1208/android-ot-br-posix

Add dump skeleton for the otdaemon_server.
Change-Id: I58caaf61c78a5790c90b73169677ed19272febd0

src/android/otdaemon_server.cpp
@@ -33,6 +33,7 @@
 #include <net/if.h>
 #include <string.h>
 
+#include <android-base/file.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
 #include <openthread/border_router.h>
@@ -484,5 +485,13 @@ exit:
     return status;
 }
 
+binder_status_t OtDaemonServer::dump(int fd, const char** args, uint32_t num_args)
+{
+    OT_UNUSED_VARIABLE(args);
+    OT_UNUSED_VARIABLE(num_args);
+    // TODO: Use ::android::base::WriteStringToFd to dump infomration.
+    fsync(fd);
+    return STATUS_OK;
+}
 } // namespace Android
 } // namespace otbr

src/android/otdaemon_server.hpp
@@ -62,8 +62,8 @@ public:
     OtDaemonServer(const OtDaemonServer &) = delete;
     void operator=(const OtDaemonServer &) = delete;
 
-    // TODO(wgtdkp): dump service info for debugging.
-    // status_t dump(int fd, const Vector<String16> &args) override;
+    // Dump information for debugging.
+    binder_status_t dump(int fd, const char** args, uint32_t num_args) override;
 
 private:
     using DetachCallback = std::function<void()>;

msft-mirror-aosp/platform.external.ot-br-posix

Bug: 295810272

src/android/otdaemon_server.cpp
@@ -33,6 +33,7 @@
 #include <net/if.h>
 #include <string.h>
 
+#include <android-base/file.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
 #include <openthread/border_router.h>
@@ -484,5 +485,15 @@ exit:
     return status;
 }
 
+binder_status_t OtDaemonServer::dump(int aFd, const char** aArgs, uint32_t aNumArgs)
+{
+    OT_UNUSED_VARIABLE(aArgs);
+    OT_UNUSED_VARIABLE(aNumArgs);
+
+    // TODO: Use ::android::base::WriteStringToFd to dump infomration.
+    fsync(aFd);
+
+    return STATUS_OK;
+}
 } // namespace Android
 } // namespace otbr

src/android/otdaemon_server.hpp
@@ -62,8 +62,8 @@ public:
     OtDaemonServer(const OtDaemonServer &) = delete;
     void operator=(const OtDaemonServer &) = delete;
 
-    // TODO(wgtdkp): dump service info for debugging.
-    // status_t dump(int fd, const Vector<String16> &args) override;
+    // Dump information for debugging.
+    binder_status_t dump(int aFd, const char** aArgs, uint32_t aNumArgs) override;
 
 private:
     using DetachCallback = std::function<void()>;